268ec42636d458fb82cbe48866fc86e517e9be5c
Rafael Falcão Gomes Jardim <rafaelfgjardim@gmail.com>
2017-10-08 14:00:46 -0300

    Updated launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
 echo "starting application..."
-sleep 10
+#sleep 2
+
+#------ argument selection -------#
+while getopts v:h: option
+do
+  case "${option}"
+    in
+    v) VERSION=${OPTARG};;
+    h) HEIGHT=${OPTARG};;
+  esac
+done
+
+echo "Number of arguments -> $#"
+echo $VERSION
+echo $HEIGHT
+
+#if [ "$1" == "test" ]; then
+#  echo "hellow"
+#fi
 
 #------ compilation of code ------#
 
 
-#------ execution ------#
+#----------- execution -----------#
 
 
-
-#------ greetings ------#
+#----------- greetings -----------#
