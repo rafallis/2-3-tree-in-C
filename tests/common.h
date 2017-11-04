d2a7259b0b3d03c9e13575a30a3ac76f8aeec4ba
Rafael Falcão Gomes Jardim <rafaelfgjardim@gmail.com>
2017-11-04 21:59:52 -0200

    Created first tests
    - allocation test
    - deallocation test

---
+++ tests/common.h
@@ -0,0 +1,10 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define PRINT_SUCCESS() printf("%s...SUCCESS\n", __PRETTY_FUNCTION__)
+
+#endif
