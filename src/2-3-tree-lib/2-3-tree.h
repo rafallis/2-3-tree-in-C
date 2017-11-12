eaeb85e1d8d31524525f0139f42cd2c6cb44bdbc
Rafael Falcão Gomes Jardim <rafaelfgjardim@gmail.com>
2017-11-12 00:47:24 -0200

    Updated insertion function
    Now, insertion is up to 1 height.
    CONSIDERING MODIFY FIRST PARAMENTER OF INSERTION TO 23tree_node NOT 23tree.

--- src/2-3-tree-lib/2-3-tree.h
+++ src/2-3-tree-lib/2-3-tree.h
@@ -15,7 +15,7 @@
 struct _23tree_node;
 typedef struct _23tree_link {
   struct _23tree_node *node;  //internal node
-  int data;
+  //int data;
 } 23tree_link;
 
 /**
@@ -49,21 +49,21 @@ typedef struct _23tree {
 /**
  * Frees space used by the 2-3 tree.
  */
-void 23tree_free(23tree *t);
+void 23tree_free(23tree *tree);
 
 /**
  * Inserts an item to 2-3 tree.
  * If the item already is in the tree, the function returns
  * the pointer to the existent item.
  */
-void *23tree_insert(23tree *t, int *item);
+void *23tree_insert(23tree *tree, int item);
 
-void *23tree_find(23tree *t, int key_item);
+void *23tree_find(23tree *tree, int key_item);
 
-void *23tree_find_min(23tree *t);
+void *23tree_find_min(23tree *tree);
 
-void *23tree_delete(23tree *t, int key_item);
+void *23tree_delete(23tree *tree, int key_item);
 
-void *23tree_delete_min(23tree *t);
+void *23tree_delete_min(23tree *tree);
 
 #endif
